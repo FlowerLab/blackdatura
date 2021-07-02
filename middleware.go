11fee01195179ee90692dbc2ea8c4f02c44d78ec
Aoang <aoang@x2ox.com>
2021-07-02 14:54:51 +0800

    Update function RecoveryWithZap

--- middleware.go
+++ middleware.go
@@ -1,11 +1,14 @@
 package blackdatura
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"os"
-	"runtime/debug"
+	"runtime"
 	"strings"
 	"time"
 
@@ -60,23 +63,27 @@ func RecoveryWithZap(l *zap.Logger) gin.HandlerFunc {
 				}
 
 				httpRequest, _ := httputil.DumpRequest(c.Request, false)
-				if brokenPipe {
-					l.Error(c.Request.URL.Path,
-						zap.Any("error", err),
-						zap.String("request", string(httpRequest)),
-					)
-					// If the connection is dead, we can't write a status to it.
-					_ = c.Error(err.(error))
-					c.Abort()
-					return
+				headers := strings.Split(string(httpRequest), "\r\n")
+				for idx, header := range headers {
+					current := strings.Split(header, ":")
+					if current[0] == "Authorization" {
+						headers[idx] = current[0] + ": *"
+					}
 				}
 
-				l.Error("[Recovery from panic]",
-					zap.Time("time", time.Now()),
-					zap.Any("error", err),
-					zap.String("request", string(httpRequest)),
-					zap.String("stack", string(debug.Stack())),
-				)
+				if brokenPipe {
+					l.Error("[Recovery]",
+						zap.Time("time", time.Now()),
+						zap.Any("error", err),
+						zap.String("request", strings.Join(headers, "\r\n")))
+				} else {
+					l.Error("[Recovery from panic]",
+						zap.Time("time", time.Now()),
+						zap.Any("error", err),
+						zap.String("request", strings.Join(headers, "\r\n")),
+						zap.String("stack", string(stack(3))),
+					)
+				}
 
 				c.AbortWithStatus(http.StatusInternalServerError)
 			}
@@ -88,11 +95,72 @@ func RecoveryWithZap(l *zap.Logger) gin.HandlerFunc {
 func Recovery(l *zap.Logger) {
 	defer func() {
 		if err := recover(); err != nil {
-			l.Error("[Recovery from panic]",
+			l.Error("[Recovery]",
 				zap.Time("time", time.Now()),
 				zap.Any("error", err),
-				zap.String("stack", string(debug.Stack())),
+				zap.String("stack", string(stack(0))),
 			)
 		}
 	}()
+}
+
+// stack returns a nicely formatted stack frame, skipping skip frames.
+func stack(skip int) []byte {
+	buf := new(bytes.Buffer) // the returned data
+	// As we loop, we open files and read them. These variables record the currently
+	// loaded file.
+	var lines [][]byte
+	var lastFile string
+	for i := skip; ; i++ { // Skip the expected number of frames
+		pc, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		// Print this much at least.  If we can't find the source, it won't show.
+		fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
+		if file != lastFile {
+			data, err := ioutil.ReadFile(file)
+			if err != nil {
+				continue
+			}
+			lines = bytes.Split(data, []byte{'\n'})
+			lastFile = file
+		}
+		fmt.Fprintf(buf, "\t%s: %s\n", function(pc), source(lines, line))
+	}
+	return buf.Bytes()
+}
+
+// source returns a space-trimmed slice of the n'th line.
+func source(lines [][]byte, n int) []byte {
+	n-- // in stack trace, lines are 1-indexed but our array is 0-indexed
+	if n < 0 || n >= len(lines) {
+		return []byte("???")
+	}
+	return bytes.TrimSpace(lines[n])
+}
+
+// function returns, if possible, the name of the function containing the PC.
+func function(pc uintptr) []byte {
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return []byte("???")
+	}
+	name := []byte(fn.Name())
+	// The name includes the path name to the package, which is unnecessary
+	// since the file name is already included.  Plus, it has center dots.
+	// That is, we see
+	//	runtime/debug.*T·ptrmethod
+	// and want
+	//	*T.ptrmethod
+	// Also the package path might contains dot (e.g. code.google.com/...),
+	// so first eliminate the path prefix
+	if lastSlash := bytes.LastIndex(name, []byte("/")); lastSlash >= 0 {
+		name = name[lastSlash+1:]
+	}
+	if period := bytes.Index(name, []byte(".")); period >= 0 {
+		name = name[period+1:]
+	}
+	name = bytes.Replace(name, []byte("·"), []byte("."), -1)
+	return name
 }
